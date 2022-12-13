d1df6b8f9e5e4f1e2f7f426f1bad0ca76cb044a6
miledesmam <miledesmam@gmail.com>
2022-12-13 00:22:57 -0600

    Threads with Runnable

---
+++ src/com/lei6mar/hilos/ejemplos/InterfaceRunnableExample.java
@@ -0,0 +1,11 @@
+package com.lei6mar.hilos.ejemplos;
+
+import com.lei6mar.hilos.ejemplos.runnable.ViajeTarea;
+
+public class InterfaceRunnableExample {
+    public static void main(String[] args) {
+        new Thread(new ViajeTarea("Isla de Pascua")).start();
+        new Thread(new ViajeTarea("Washington")).start();
+        new Thread(new ViajeTarea("Chipre")).start();
+    }
+}
