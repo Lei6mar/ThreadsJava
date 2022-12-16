59f180a4c231e83cb2afc952c37a071ae221f1b3
miledesmam <miledesmam@gmail.com>
2022-12-15 23:48:51 -0600

    thread join

--- src/com/lei6mar/hilos/ejemplos/InterfaceRunnableJava8Example.java
+++ src/com/lei6mar/hilos/ejemplos/InterfaceRunnableJava8Example.java
@@ -3,7 +3,8 @@ package com.lei6mar.hilos.ejemplos;
 import com.lei6mar.hilos.ejemplos.runnable.ViajeTarea;
 
 public class InterfaceRunnableJava8Example {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Thread main = Thread.currentThread();
         Runnable viaje = () ->{
                 for(int i = 0; i<10; i++){
                     System.out.println(i + " - " + Thread.currentThread().getName());
@@ -14,11 +15,25 @@ public class InterfaceRunnableJava8Example {
                     }
                 }
                 System.out.println("Finalmente me voy de viaje a: "+ Thread.currentThread().getName());
+            System.out.println(main.getState());
         };
 
 
-        new Thread(viaje, "Isla de Pascua").start();
-        new Thread(viaje,"Washington").start();
-        new Thread(viaje,"Chipre").start();
+        Thread v1 = new Thread(viaje, "Isla de Pascua");
+        Thread v2 = new Thread(viaje,"Washington");
+        Thread v3 = new Thread(viaje,"Chipre");
+        Thread v4 = new Thread(viaje,"Jon kon");
+
+        v1.start();
+        v2.start();
+        v3.start();
+        v4.start();
+        v1.join();
+        v2.join();
+        v3.join();
+        v4.join();
+
+        Thread.sleep(1000);
+        System.out.println("Continuando con la ejecución del método main:  " + main.getName());
     }
 }
